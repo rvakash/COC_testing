1a36f81e13f735679b606ccc6ec0812607b9c62e
rvakash <rvakash@gmail.com>
2015-04-22 14:55:49 +0530

    MainActivity (with Bluetooth on/off and commented Tony Dicola's BLE)

--- app/src/main/java/com/example/rvakash/coc_testing/Third_Activity.java
+++ app/src/main/java/com/example/rvakash/coc_testing/Third_Activity.java
@@ -1,6 +1,8 @@
 package com.example.rvakash.coc_testing;
 
 import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.content.IntentFilter;
 import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -16,6 +19,8 @@ public class Third_Activity extends ActionBarActivity {
 
     Button homebutton,templatesbutton,settingsbutton;
     ImageButton bluetooth;
+    BluetoothAdapter btAdapter;
+    TextView statusUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +30,23 @@ public class Third_Activity extends ActionBarActivity {
         homebutton = (Button) findViewById(R.id.homebutton);
         settingsbutton = (Button) findViewById(R.id.settingsbutton);
         bluetooth = (ImageButton) findViewById(R.id.imageButton);
+        //Check if bluetooth is enabled
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter.isEnabled())
+        {
+            //To change the image
+            bluetooth = (ImageButton) findViewById(R.id.imageButton);
+            bluetooth.setImageResource(R.drawable.bluetooth_col);
+            Toast toast = new Toast(getApplicationContext());
+            Toast.makeText(Third_Activity.this, "Bluetooth is already turned on!", toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast toast = new Toast(getApplicationContext());
+            Toast.makeText(Third_Activity.this, "Bluetooth is not on. Click on Bluetooth to turn it on", toast.LENGTH_LONG).show();
 
-        Toast toast = new Toast(getApplicationContext());
-        Toast.makeText(Third_Activity.this, "Click on Bluetooth to connect to the display", toast.LENGTH_SHORT).show();
+        }
+
         homebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +54,7 @@ public class Third_Activity extends ActionBarActivity {
                 startActivity(home);
             }
         });
+
         templatesbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,15 +62,26 @@ public class Third_Activity extends ActionBarActivity {
                 startActivity(templates);
             }
         });
+
         bluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //To change the image
-                bluetooth = (ImageButton) findViewById(R.id.imageButton);
-                bluetooth.setImageResource(R.drawable.bluetooth_col);
-                Toast toast = new Toast(getApplicationContext());
-                Toast.makeText(Third_Activity.this, "Connected!", toast.LENGTH_SHORT).show();
-
+                if (btAdapter.isEnabled())
+                {
+                    btAdapter.disable();
+                    Toast toast = new Toast(getApplicationContext());
+                    Toast.makeText(Third_Activity.this, "Bluetooth is turned off!", toast.LENGTH_LONG).show();
+                    bluetooth.setImageResource(R.drawable.bluetooth_grey);
+                }
+                else {
+                    String actionStateChanged = BluetoothAdapter.ACTION_STATE_CHANGED;
+                    String actionRequestEnable = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+                    IntentFilter filter = new IntentFilter(actionStateChanged);
+                    startActivityForResult(new Intent(actionRequestEnable), 0);
+                    bluetooth.setImageResource(R.drawable.bluetooth_col);
+                    Toast toast = new Toast(getApplicationContext());
+                    Toast.makeText(Third_Activity.this, "Bluetooth is turned on!", toast.LENGTH_LONG).show();
+                }
             }
         });
     }
